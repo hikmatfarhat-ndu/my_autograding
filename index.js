82b64f4a9cb6e889065e74c33f1ed93cd9b870d8
hikmat farhat <hfarhat@ndu.edu.lb>
2021-02-08 12:21:07 +0200

    removed logging

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { runInContext } = require('vm');
 import {run} from './src/autograding'
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  //const nameToGreet = core.getInput('who-to-greet');
+  //console.log(`Hello ${nameToGreet}!`);
+  //const time = (new Date()).toTimeString();
+  //core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
   run();
 } catch (error) {
   core.setFailed(error.message);
